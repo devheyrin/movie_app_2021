b7370facd94cb906ee5312d65b11b964392c186b
devheyrin <beforerin@gmail.com>
2021-04-06 01:34:16 +0900

    Add Component LifeCycle

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 
 
 class App extends React.Component{
+
   state = {
     count: 0
   };
@@ -15,7 +16,19 @@ class App extends React.Component{
     this.setState(current => ({count: current.count - 1}));
   };
 
+  componentDidMount(){
+    console.log("Component Rendered")
+  }
+  componentDidUpdate(){
+    console.log("Component Updated")
+  }
+  componentWillUnmount(){
+    console.log("Goodbye Cruel world")
+  }
+
+
   render(){
+    console.log("I'm rendering")
     return (<div>
       <h1>The number is: {this.state.count}</h1>
       <button onClick={this.add}>Add</button>
